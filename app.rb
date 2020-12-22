fcb84ddef8252c6cf8c493315a8723f0f4d785b3
Julian <julianrosenthal@yahoo.com>
2020-12-22 15:33:37 -0800

    commit tree 6faaac24d3ea9aab255cde34b4d081fe8444068e

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require_relative 'config/environment'
-
+# main app controller
 class App < Sinatra::Base
   configure do
     enable :sessions unless test?
-    set :session_secret, "secret"
+    set :session_secret, 'secret'
   end
 
   before do
@@ -11,19 +11,22 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    "Welcome to Sinatra Sessions! In this lab, we will be learning about the general principles behind a sessions cookie. In order to proceed, let's go to the '/first_exercise' path."
+    'Welcome to Sinatra Sessions! In this lab, we will be learning about the general principles'\
+    " behind a sessions cookie. In order to proceed, let's go to the '/first_exercise' path."
   end
 
   get '/first_exercise' do
-    "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, navigate to the '/set' path."
+    "Your first exercise will be to set your session key-value pair.\n"\
+    "In the route: get '/set', write a line of code that sets the :foo key\n"\
+    "of the session hash equal to 'hello'. Then, navigate to the '/set' path."
   end
 
   get '/set' do
-    # set the :foo key of the session hash equal to 'hello' here!
+    session[:foo] = 'hello'
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
-      "Session value has not been set!"
+      'Session value has not been set!'
     end
   end
 
@@ -36,26 +39,29 @@ class App < Sinatra::Base
   end
 
   get '/set_session' do
-    #set session id here
+    session[:id] = 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
     else
-      "Session ID has not been set!"
+      'Session ID has not been set!'
     end
   end
 
   get '/fetch_session_id' do
-    "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
+    "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for 
+    further instructions.\nThen, navigate to the '/logout' path."
   end
 
   get '/logout' do
-    #clear session hash here
+    session.clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
   get '/finish' do
-    "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
+    "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data 
+    on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID 
+    attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
   end
 end
